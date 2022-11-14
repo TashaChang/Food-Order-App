7e4b1c4356e1e766df849e6cbf5bbd1712c1bb96
TashaChang <jhih525@gmail.com>
2022-11-14 17:03:57 +0800

    Complete headercartbutton animation

--- src/Components/Layout/HeaderCartButton.jsx
+++ src/Components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "../style.css/HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnAnimation, setbtnAnimation] = useState(false);
   const ctx = useContext(CartContext);
+  const { items } = ctx;
 
-  const buttonNumber = ctx.items.reduce((accumulate, currentnumber) => {
+  const buttonNumber = items.reduce((accumulate, currentnumber) => {
     return accumulate + currentnumber.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnAnimation ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setbtnAnimation(true);
+
+    const timer = setTimeout(() => {
+      setbtnAnimation(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.click} className={classes.button}>
+    <button onClick={props.click} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
